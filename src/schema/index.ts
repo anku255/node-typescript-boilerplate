46f4dcbf80b245de9e0d486ffa235dea1423f44f
Ankit Tiwari <ankitt255@gmail.com>
2020-11-07 16:53:02 +0530

    Use tsc instead of ts-node

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,6 +2,8 @@ import path from 'path';
 import { loadFilesSync } from '@graphql-tools/load-files';
 import { mergeTypeDefs } from '@graphql-tools/merge';
 
-const typesArray = loadFilesSync(path.join(__dirname, './types'));
+console.log('path', path.join(__dirname, '../../../src/schema/types'));
+
+const typesArray = loadFilesSync(path.join(__dirname, '../../../src/schema/types'));
 
 export default mergeTypeDefs(typesArray);
